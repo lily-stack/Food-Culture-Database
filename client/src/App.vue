cfc722286420748b80e0942c47e96a5c7f17f919
Preston Ford <preston.y.ford@gmail.com>
2025-11-07 16:45:36 -0700

    Add tailwindCSS

--- client/src/App.vue
+++ client/src/App.vue
@@ -6,6 +6,9 @@
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
   </p>
+  <div class="flex w-[300px] h-[300px] bg-red-500">
+
+  </div>
 </template>
 
 <style scoped></style>
