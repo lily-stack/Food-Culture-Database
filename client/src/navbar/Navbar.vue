c52367ee06038234cf78e2910e1c0a1b947bdec1
Preston Ford <preston.y.ford@gmail.com>
2025-11-08 19:08:42 -0700

    Responsive navbar

--- client/src/navbar/Navbar.vue
+++ client/src/navbar/Navbar.vue
@@ -3,11 +3,11 @@
 		<div
 			class="bg-white grow shadow-lg rounded-lg h-12 border border-gray-200 flex gap-2 items-center justify-between px-4 py-2 text-gray-600 transition-colors font-semibold">
 			<div class="basis-0 grow text-xl font-semibold select-none font-[Merriweather] text-black">
-				<span class="cursor-pointer">
+				<span class="cursor-pointer whitespace-nowrap">
 					<RouterLink to="/">Recipes Worldwide</RouterLink>
 				</span>
 			</div>
-			<div class="hidden sm:flex gap-8">
+			<div class="hidden sm:flex gap-2 md:gap-8">
 				<NavbarOption path="/">
 					<i class="fa-solid fa-house"></i>
 					Home
@@ -21,8 +21,8 @@
 					Search
 				</NavbarOption>
 			</div>
-			<div class="basis-0 grow text-right">
-				<span class="cursor-pointer whitespace-nowrap hover:text-gray-800">
+			<div class="ml-1 basis-0 grow text-right truncate">
+				<span class="cursor-pointer hover:text-gray-800">
 					<i class="fa-regular fa-user"></i>
 					Username
 				</span>
