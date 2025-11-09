2e0dace4e306b5c2823b701e08edecd776948b7b
Preston Ford <preston.y.ford@gmail.com>
2025-11-09 15:28:54 -0700

    Add RouterLink to RecipeListItem

--- client/src/pages/map/RecipeList.vue
+++ client/src/pages/map/RecipeList.vue
@@ -25,12 +25,12 @@ const fakeRecipes = reactive<Recipe[]>([
 	{
 		title: "Bibimbap",
 		imageSrc: "https://cdn.apartmenttherapy.info/image/upload/f_jpg,q_auto:eco,c_fill,g_auto,w_1500,ar_4:3/k%2FPhoto%2FRecipes%2F2024-03-bimbimbap%2Fbibimbap-074",
-		countries: ["KR"]
+		countries: ["KR", "KP"]
 	},
 	{
 		title: "Jajangmyeon",
 		imageSrc: "https://www.beyondkimchee.com/wp-content/uploads/2023/11/jajangmyeon-black-bean-noodles-thumbnail.jpg",
-		countries: ["KR", "CN"]
+		countries: ["KR", "KP", "CN"]
 	},
 ])
 </script>
--- client/src/pages/map/RecipeListItem.vue
+++ client/src/pages/map/RecipeListItem.vue
@@ -1,31 +1,35 @@
 <template>
-	<div class="border border-gray-200 rounded-lg shadow-md p-3 flex justify-between gap-2 cursor-pointer hover:bg-gray-50 transition-colors">
-		<div class="flex gap-3">
-			<img :src="imgSrc" class="w-24 h-24 rounded-md object-cover border border-gray-200" />
-			<div class="flex flex-col">
-				<h4>{{ title }}</h4>
-				<div class="text-xs text-gray-600 flex flex-row gap-2">
-					<div title="Preparation time">
-						<i class="fa-regular fa-clock"></i>
-						<span class="ml-0.5">1h 30m</span>
+	<RouterLink to="recipes/recipeid">
+		<div
+			class="border border-gray-200 rounded-lg shadow-md p-3 flex justify-between gap-2 cursor-pointer hover:bg-gray-50 transition-colors">
+			<div class="flex gap-3">
+				<img :src="imgSrc" class="w-24 h-24 rounded-md object-cover border border-gray-200" />
+				<div class="flex flex-col">
+					<h4>{{ title }}</h4>
+					<div class="text-xs text-gray-600 flex flex-row gap-2">
+						<div title="Preparation time">
+							<i class="fa-regular fa-clock"></i>
+							<span class="ml-0.5">1h 30m</span>
+						</div>
+						<div title="Number of servings">
+							<i class="fa-regular fa-user"></i>
+							<span class="ml-0.5">4</span>
+						</div>
 					</div>
-					<div title="Number of servings">
-						<i class="fa-regular fa-user"></i>
-						<span class="ml-0.5">4</span>
-					</div>
+					<p class="mt-1.5 text-gray-800 line-clamp-2 text-sm">
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean auctor finibus lectus, vel
+						aliquam
+						massa dignissim ut. Maecenas tristique tellus nec erat porta, sit amet pretium enim consectetur.
+						Suspendisse potenti. Nulla sed pellentesque sapien.</p>
 				</div>
-				<p class="mt-1.5 text-gray-800 line-clamp-2 text-sm">
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean auctor finibus lectus, vel aliquam
-					massa dignissim ut. Maecenas tristique tellus nec erat porta, sit amet pretium enim consectetur.
-					Suspendisse potenti. Nulla sed pellentesque sapien.</p>
+			</div>
+			<div class="flex">
+				<div class="m-auto text-2xl text-gray-800">
+					<i class="fa-solid fa-angle-right"></i>
+				</div>
 			</div>
 		</div>
-		<div class="flex">
-			<div class="m-auto text-2xl text-gray-800">
-				<i class="fa-solid fa-angle-right"></i>
-			</div>
-		</div>
-	</div>
+	</RouterLink>
 </template>
 
 <script setup lang="ts">
